e2a6916fc82cc21061989b2aaac96e36cfe3b570
Matthew Else <Matthew.Else@arm.com>
2016-09-05 10:29:33 +0100

    Add support for targets that output .hex files

--- tools/mbed-js.sh
+++ tools/mbed-js.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+set -e
 
-cjsc index.js -o /workspace/js/main.js && cd /workspace/ && js2c && mbed-cli compile -t GCC_ARM -m $1 && mv .build/$1/GCC_ARM/workspace.bin /mbed_home/mbed-js.bin
+cjsc index.js -o /workspace/js/main.js && cd /workspace/ && js2c && mbed-cli compile -t GCC_ARM -m $1
+
+if [ -f ./.build/$1/GCC_ARM/workspace.bin ]; then
+mv .build/$1/GCC_ARM/workspace.bin /mbed_home/mbed-js.bin
+elif [ -f ./.build/$1/GCC_ARM/workspace.hex ]; then
+mv .build/$1/GCC_ARM/workspace.hex /mbed_home/mbed-js.hex
+else
+echo Unable to find output binary.
+exit 1
+fi
+
+
